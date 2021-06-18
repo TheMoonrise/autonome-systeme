bad045811eaffe06f89510d9c563ca531946d6e8
Mariam <mariam_ali1000@yahoo.de>
2021-06-18 08:37:43 +0200

    add play crawler

---
+++ main/play_crawler_sac.py
@@ -0,0 +1,59 @@
+from src.sac.sac_parameters import Parameters
+from src.sac.sac_actor_critic import PolicyNetwork
+from src.utils.domain import Domain
+from src.utils.wrapper import CrawlerWrapper
+import torch
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+
+# create a crawler environment and wrap it in the gym wrapper
+env = Domain().environment()
+env = CrawlerWrapper(env)
+
+
+# define the hyper parameters
+params = Parameters(env.observation_space_size, env.action_space_size)
+
+inputs = env.observation_space_size
+outputs = env.action_space_size
+
+hidden_dim = 512
+value_lr = params.value_lr
+soft_q_lr = params.soft_q_lr
+policy_lr = params.policy_lr
+replay_buffer_size = params.replay_buffer_size
+batch_size = params.batch_size
+soft_tau = params.soft_tau
+gamma = params.gamma
+max_frames = params.max_frames
+max_steps = params.max_steps
+
+name = "testrun"
+
+policy_net = PolicyNetwork(inputs, outputs, hidden_dim, name, device).to(device)
+
+policy_net.load_state_dict(torch.load('../models/sac/temp/crawler2300', map_location=device))
+policy_net.eval()
+
+reward_mean = 0
+
+for i in range(20):
+    state = env.reset()
+    reward_total = 0
+    done = False
+
+    while not done:
+        state = torch.FloatTensor(state).unsqueeze(0).to(device)
+        dist, _ = policy_net(state)
+        action = policy_net.get_action(state).detach()
+
+        state_next, reward, done, _ = env.step(action.numpy())
+        state = state_next.squeeze()
+
+        reward_total += reward[0]
+        env.render()
+
+    reward_mean += reward_total
+    print("Mean Reward Episode ", i, reward_mean / 20)
