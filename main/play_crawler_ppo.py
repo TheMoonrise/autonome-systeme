4da78e76e4e524b9696eb5ae8702de854017844c
Patrick Tamunjoh <38542007+TheMoonrise@users.noreply.github.com>
2021-06-28 19:51:26 +0200

    add argparse to play ppo and add more models and parameters

--- main/play_crawler_ppo.py
+++ main/play_crawler_ppo.py
@@ -1,4 +1,4 @@
-import sys
+from argparse import ArgumentParser
 
 from src.ppo.ppo_parameters import Parameters
 from src.ppo.ppo_actor_critic_crawler import ActorCriticCrawler
@@ -6,16 +6,25 @@ from src.ppo.ppo_training import TrainAndEvaluate
 from src.utils.domain import Domain
 from src.utils.wrapper import CrawlerWrapper
 
+parser = ArgumentParser(description='The argument mining prediction.')
+
+parser.add_argument('--model', type=str, help='The name of the model to load.', default='crawler-v1')
+parser.add_argument('--params', type=str, help='The parameter file for the model.')
+parser.add_argument('--speed', type=float, help='Define the speed at which the simulation runs.', default=1)
+
+args = parser.parse_args()
+
 # create a crawler environment and wrap it in the gym wrapper
-env = Domain().environment()
+env = Domain().environment(args.speed)
 env = CrawlerWrapper(env)
 
 # define the hyper parameters
 params = Parameters(env.observation_space_size, env.action_space_size)
+if args.params is not None: params.load(args.params)
 
 # create a model and load the parameters
 # use the first commandline argument for the model if given
-model = ActorCriticCrawler(params, 'crawler-v1' if len(sys.argv) < 2 else sys.argv[1])
+model = ActorCriticCrawler(params, args.model)
 model.load()
 
 # run the training loop
