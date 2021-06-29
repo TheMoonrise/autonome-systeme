33dba1b38c65205676d6d57a6e065c73ca53fffe
Patrick Tamunjoh <38542007+TheMoonrise@users.noreply.github.com>
2021-06-29 15:56:05 +0200

    add slipperiness and steepness to the unity domain

--- main/train_crawler_ppo.py
+++ main/train_crawler_ppo.py
@@ -21,14 +21,16 @@ parser.add_argument('--params', type=str, help='The parameter file for the model
 parser.add_argument('--tag', type=str, help='An additional tag for identifying this run.')
 
 parser.add_argument('--speed', type=float, help='Define the speed at which the simulation runs.', default=1)
-parser.add_argument('--quality', type=float, help='Define the quality of the physics simulation', default=1)
+parser.add_argument('--quality', type=float, help='Define the quality of the physics simulation.', default=1)
+parser.add_argument('--slipperiness', type=float, help='Define how slippery the ground is [0, 1]', default=0)
+parser.add_argument('--steepness', type=float, help='Define how steep and uneven the terrain is [0, 1]', default=0)
 parser.add_argument('--no-window', help='Hides the simulation window.', action='store_true')
 parser.add_argument('--no-mlflow', help='Disables mlflow logging for the run.', action='store_true')
 
 args = parser.parse_args()
 
 # create a crawler environment and wrap it in the gym wrapper
-env = Domain().environment(args.speed, args.quality, args.no_window)
+env = Domain().environment(args.speed, args.quality, args.no_window, args.slipperiness, args.steepness)
 env = CrawlerWrapper(env)
 
 # load environment variables
---
+++ unity/Crawler/Assets/Custom/Scripts/WorldConfig.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class WorldConfig : MonoBehaviour {
+
+    [SerializeField] private PhysicMaterial groundPhysicsMat;
+    [SerializeField] private Material groundMat;
+    [SerializeField] private Color defaultColor, iceColor;
+
+    private float current_slipperiness = -1;
+    private float current_steepness = -1;
+
+    private void Awake() {
+
+        UpdateSteepness(0);
+        UpdateSlipperiness(0);
+    }
+
+    void Update() {
+
+        var eparams = Academy.Instance.EnvironmentParameters;
+
+        var slipperiness = eparams.GetWithDefault("slipperiness", 0);
+        UpdateSlipperiness(slipperiness);
+
+        var steepness = eparams.GetWithDefault("steepness", 0);
+        UpdateSteepness(steepness);
+    }
+
+    private void UpdateSlipperiness(float slipperiness) {
+
+        if (current_slipperiness == slipperiness) return;
+        current_slipperiness = slipperiness;
+
+        var color = Color.Lerp(defaultColor, iceColor, slipperiness);
+        groundMat.SetColor("_Color", color);
+
+        var friction = Mathf.Lerp(.6f, 0f, slipperiness);
+        groundPhysicsMat.dynamicFriction = friction;
+        groundPhysicsMat.staticFriction = friction;
+    }
+
+    private void UpdateSteepness(float steepness) {
+
+        if (current_steepness == steepness) return;
+        current_steepness = steepness;
+
+        foreach (var g in FindObjectsOfType<UnevenGround>()) g.Generate(steepness);
+    }
+}
