169bf171da44ecc789bc7b487e7d404ba2f5eadb
Mariam <mariam_ali1000@yahoo.de>
2021-07-03 22:48:16 +0200

    add parser args and comments

--- main/train_crawler_sac.py
+++ main/train_crawler_sac.py
@@ -14,13 +14,24 @@ from src.utils.wrapper import CrawlerWrapper
 
 # parse argument from cmd
 parser = ArgumentParser(description='sac training crawler')
-parser.add_argument('--fname', type=str, help='the name under which the trained model is stored', default="test_run")
+parser.add_argument('--runs', type=int, help='how many times the training will be performed.', default=1)
+parser.add_argument('--model', type=str, help='The name of the model to load.')
 parser.add_argument('--params', type=str, help='The parameter file for the model.')
-parser.add_argument('--speed', type=float, help='The time scale of the simulation.', default=1)
+parser.add_argument('--tag', type=str, help='An additional tag for identifying this run.')
+
+parser.add_argument('--speed', type=float, help='Define the speed at which the simulation runs.', default=1)
+parser.add_argument('--quality', type=int, help='Define the quality of the simulation.', default=0)
+parser.add_argument('--slipperiness', type=float, help='Define how slippery the ground is [0, 1]', default=0)
+parser.add_argument('--steepness', type=float, help='Define how steep and uneven the terrain is [0, 1]', default=0)
+parser.add_argument('--hue', type=float, help='Defines the color hue of the crawler [0, 360]', default=50)
+
+parser.add_argument('--no-window', help='Hides the simulation window.', action='store_true')
+parser.add_argument('--no-mlflow', help='Disables mlflow logging for the run.', action='store_true')
+
 args = parser.parse_args()
 
 # create a crawler environment and wrap it in the gym wrapper
-env = Domain().environment(time_scale=args.speed)
+env = Domain().environment(args.speed, args.quality, args.no_window, args.slipperiness, args.steepness, args.hue)
 env = CrawlerWrapper(env)
 
 # load environment variables
@@ -222,16 +233,11 @@ def sac_update(batch_size, gamma, soft_tau, episode):
 
 # Training Value Function
     # Choose minimum of Q-functions for the value gradient and policy gradient
-
     predicted_new_q_value = torch.min(soft_q_net1(state, new_action), soft_q_net2(state, new_action))
 
-    # calculate the target value for the Q-value network
     # log_prob: entropy of the policy function π (measured here by the negative log of the policy function)
-    # predicted_new_q_value: prediction of our value network
-    # yt(r,s′,d)=r+γ(minj=1,2Qϕtarg,j(s′,a~′)−αlogπθ(a~θ(s,ξ)|s))
+    # See equation 6 from [1]
     target_value_func = predicted_new_q_value - log_prob
-
-    # for reshaping predicted_value
     reshape_v = torch.zeros(batch_size, 10, outputs).to(device)
     predicted_value = predicted_value - reshape_v
 
@@ -244,6 +250,7 @@ def sac_update(batch_size, gamma, soft_tau, episode):
     mlflow.log_metric('value loss', value_loss.item(), step=episode)
 
 # Training Policy Function
+    # policy optimization with equation 10 from [1]: maxθE(s∼D,ξ∼N)[minj=1,2Qϕj(s,a~θ(s,ξ))−αlogπθ(a~θ(s,ξ)|s)]
     policy_loss = (log_prob - predicted_new_q_value).mean()
 
     policy_optimizer.zero_grad()
@@ -252,7 +259,7 @@ def sac_update(batch_size, gamma, soft_tau, episode):
 
     mlflow.log_metric('loss', policy_loss.item(), step=episode)
 
-    # Soft update model parameters. θ_target = τ*θ_local + (1 - τ)*θ_target
+    # Soft update model parameters of the value function. θ_target = τ*θ_local + (1 - τ)*θ_target
     for target_param, param in zip(target_value_net.parameters(), value_net.parameters()):
         target_param.data.copy_(
             target_param.data * (1.0 - soft_tau) + param.data * soft_tau
